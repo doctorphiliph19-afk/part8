4fb3d013219254502cc2679631ae894326211634
Student <student@sirafrica.org>
2026-08-17 15:15:26 +0300

    Exercise 8.3: implement allAuthors query

---
+++ library-backend/resolvers.js
@@ -0,0 +1,131 @@
+// In-memory data storage
+let authors = [
+  {
+    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+    name: "Robert Martin",
+    born: 1952,
+  },
+  {
+    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+    name: "Martin Fowler",
+    born: 1963,
+  },
+  {
+    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+    name: "Fyodor Dostoevsky",
+    born: 1821,
+  },
+  {
+    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+    name: "Joshua Kerievsky",
+  },
+  {
+    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+    name: "Sandi Metz",
+  },
+]
+
+let books = [
+  {
+    title: "Clean Code",
+    published: 2008,
+    author: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring"],
+  },
+  {
+    title: "Agile software development",
+    published: 2002,
+    author: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+    genres: ["agile", "patterns", "design"],
+  },
+  {
+    title: "Refactoring, edition 2",
+    published: 2018,
+    author: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring"],
+  },
+  {
+    title: "Refactoring to patterns",
+    published: 2008,
+    author: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring", "patterns"],
+  },
+  {
+    title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
+    published: 2012,
+    author: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+    genres: ["refactoring", "design"],
+  },
+  {
+    title: "Crime and punishment",
+    published: 1866,
+    author: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+    genres: ["classic", "crime"],
+  },
+  {
+    title: "Demons",
+    published: 1872,
+    author: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+    genres: ["classic", "revolution"],
+  },
+]
+
+const resolvers = {
+  Query: {
+    bookCount: () => books.length,
+    authorCount: () => authors.length,
+    allBooks: (root, args) => {
+      if (args.genre) {
+        return books.filter(book => book.genres.includes(args.genre))
+      }
+      return books
+    },
+    allAuthors: () => authors,
+  },
+  Book: {
+    author: (root) => authors.find(a => a.id === root.author),
+  },
+  Author: {
+    bookCount: (root) =>
+      books.filter(book => book.author === root.id).length,
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      let author = authors.find(a => a.name === args.author)
+      if (!author) {
+        author = {
+          id: Math.random().toString(36),
+          name: args.author,
+        }
+        authors.push(author)
+      }
+
+      const book = {
+        title: args.title,
+        published: args.published,
+        author: author.id,
+        genres: args.genres,
+        id: Math.random().toString(36),
+      }
+      books.push(book)
+      return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if (!author) {
+        return null
+      }
+      author.born = args.setBornTo
+      return author
+    },
+  },
+}
+
+module.exports = resolvers
---
+++ library-backend/schema.js
@@ -0,0 +1,51 @@
+const typeDefs = `
+  type Book {
+    title: String!
+    published: Int!
+    author: Author!
+    genres: [String!]!
+    id: ID!
+  }
+
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+
+  type User {
+    username: String!
+    id: ID!
+  }
+
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(genre: String): [Book!]!
+    allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
+    ): Book!
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
+    createUser(
+      username: String!
+      password: String!
+    ): User
+    login(
+      username: String!
+      password: String!
+    ): String
+  }
+`
+
+module.exports = typeDefs
